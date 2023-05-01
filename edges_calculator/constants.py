1abb9b591f754ce50deeeb7bc46db77ac875f2f5
nicolasplantier <91543973+nicolasplantier@users.noreply.github.com>
2023-05-01 15:46:34 +0200

    Delete edges_calculator directory

--- edges_calculator/constants.py
+++
@@ -1,7 +0,0 @@
-"""
-Here, we define all the constants that will be used for the edges_calculator function
-"""
-
-PLOT_SIZE = 1500
-DELTA = 0.011 
-KERNEL_SIZE_MAX = 4
--- edges_calculator/functions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import math
-from tqdm import tqdm
-import laspy 
-import os 
-import datashader as ds 
-import scipy 
-import matplotlib.pyplot as plt 
