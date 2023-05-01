882d0bed00391aa80616c3f54cb8a3117cc817ec
nicolasplantier <91543973+nicolasplantier@users.noreply.github.com>
2023-05-01 15:47:21 +0200

    Delete planity_calculator directory

--- planity_calculator/constants.py
+++
@@ -1,5 +0,0 @@
-"""
-This file is here to give a certain number of variables used in the planity_calculator function. 
-"""
-
-PX_SIZE = 0.08   # This is used when creating the voxel column in a cube where each voxel has size PX_SIZE * PX_SIZE * PX_SIZE
